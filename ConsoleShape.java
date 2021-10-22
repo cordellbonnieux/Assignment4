106a5f4ad0f94cad212cb567c97ea4e94af8c82c
Cordell <cordellbonnieux@gmail.com>
2021-10-22 13:41:48 -0700

    Exercise 1 complete.

--- ConsoleShape.java
+++ ConsoleShape.java
@@ -1,4 +1,29 @@
 
 public abstract class ConsoleShape {
-
+	private boolean filledIn;
+	
+	public ConsoleShape(boolean filled) {
+		this.filledIn = filled;
+	}
+	
+	public ConsoleShape() {
+		this(true);
+	}
+	
+	public boolean isFilledIn() {
+		return this.filledIn;
+	}
+	
+	public void setFilledIn(boolean filled) {
+		this.filledIn = filled;
+	}
+	
+	public abstract double getArea();
+	
+	public abstract double getPerimeter();
+	
+	@Override
+	public String toString() {
+		return new String("*** Console Shape Object *** \nFilled In: " + this.filledIn);
+	}
 }
