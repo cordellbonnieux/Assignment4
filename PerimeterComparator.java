a5d35ee7f5d1f8a9e28a7f1fefcf9f62e98fd883
Cordell <cordellbonnieux@gmail.com>
2021-10-24 21:53:14 -0700

    Created PerimeterComparator, to be used to compare ConsoleShapes

---
+++ PerimeterComparator.java
@@ -0,0 +1,20 @@
+import java.util.Comparator;
+/**
+ * PerimeterComparator
+ * Compares ConsoleShapes by perimeter
+ * @author Cordell Bonnieux
+ *
+ */
+public class PerimeterComparator implements Comparator<ConsoleShape> {
+	
+	/**
+	 * Compare
+	 * Compares ConsoleShapes by perimeter
+	 * @return int - 0 if one == two; negative if x < y ; positive if x > y
+	 */
+	@Override
+	public int compare(ConsoleShape one, ConsoleShape two) {
+		return Integer.compare(one.getPerimeter(), two.getPerimeter());
+	}
+
+}
